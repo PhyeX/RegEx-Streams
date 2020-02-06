acfe998e2b1391c6f2db487ffc8d09f17ea5995e
PhyeX <45934056+PhyeX@users.noreply.github.com>
2020-02-06 17:32:35 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -99,9 +99,9 @@ public class Main {
 
      String string = "1203x.";
      String string2 = "32xAx,";
-
      System.out.println(string.matches("^[123][120][xs0][30Aa][xsu][.,]$"));
      System.out.println(string2.matches("^[123][120][xs0][30Aa][xsu][.,]$"));
+        
      // Output:true
      // Output:true
 
@@ -117,9 +117,9 @@ public class Main {
     // * Fourth character should not be a whitespace character ( \r, \n, \t, \f or <space> ).
     // * Fifth character should not be a uppercase vowel (AEIOU).
     // * Sixth character should not be a . or , symbol.
+        
      String string = "think?";
      String string2 = "9adab=";
-
      System.out.println(string.matches("^[^\\d][^aeiou][^bcDF][^\\W][^AEIOU][^.,]$"));
      System.out.println(string2.matches("^[^\\d][^aeiou][^bcDF][^\\W][^AEIOU][^.,]$"));
 
@@ -165,13 +165,93 @@ public class Main {
      String string2 = "3333333333aaaaaaaaaa2222222222bbbbbbbbbb19 45";
      System.out.println(string.matches("^[a-zA-z246810]{40}[13579\\W]{5}$"));
      System.out.println(string2.matches("^[a-zA-z246810]{40}[13579\\W]{5}$"));
+        
      // Output: true
      // Output: false
 
      //{x}
      //The tool {x} will match exactly x repetitions of character/character class/groups.
 
+       // Example 11
 
+     // Task:You have a test string S.
+    // Your task is to write a regex that will match S using the following conditions:
+    // *S should begin with 1 or 2 digits.
+    // After that,S should have 3 or more letters(both lowercase and uppercase)
+    // Then S should end with up to 3 . symbol(s).You can end with 0 to 3 . symbol(s),inclusively.
+
+       String string = "3threeormorealphabets.";
+       String string2 = "12hooppallaa...";
+       System.out.println(string.matches("^\\d{1,2}[a-zA-Z]{3,}[.]{0,3}$"));
+       System.out.println(string2.matches("^\\d{1,2}[a-zA-Z]{3,}[.]{0,3}$"));
+
+      // Output: true
+      // Output: true
+      // The {x,y} tool will match between x and y (both inclusive) repetitions of character/character class/group.
+
+      //Example 12
+    // Task:You have a test string S.
+    // S should begin with 2 or more digits.
+    // After that,S should have 0 or more lowercase letters.
+    // S should end with 0 or more uppercase letters.
+
+        String string = "14";
+        String string2 = "1500aaXX";
+        System.out.println(string.matches("^\\d{2,}[0a-z]{0,}[0A-Z]{0,}$"));
+        System.out.println(string2.matches("^\\d{2,}[0a-z]{0,}[0A-Z]{0,}$"));
+        // Output: true
+        // Output: true
+      //   ^\\d{2,}[0a-z]{0,}[0A-Z]{0,}$ == ^\\d{2,}[0a-z]*[0A-Z]*$
+      // * Expression
+      // The * tool will match zero ore more repetitions of character/character class/group.Mean == {0,}
+
+     //Example 13
+     // Task:Your task is to write a regex that will match S using the following conditions:
+     // S should begin with 1 or more digits.
+     // After that,S should have 1 or mode uppercase letters.
+     // S should end with 1 or more lowercase letters.
+
+     String string = "1Qa";
+     String string2 = "1122QQww";
+     System.out.println(string.matches("^\\d+[A-Z]+[a-z]+$"));
+     System.out.println(string2.matches("^\\d+[A-Z]+[a-z]+$"));
+        // Output: true
+        // Output: true
+
+     // + Expression
+     // The + tool will match one ore more repetitions of character/character class/group. Mean ==  {1,0}
+
+     // Example 14
+     // Task: Write a RegEx to match a test string,S,under the following conditions:
+     // * S should consist of only lowercase and uppercase letters(no numbers of symbols)
+     // * S should end in s.
+
+    String string = "Kites";
+    String string2 = "mAchIneS";
+    System.out.println(string.matches("^[a-zA-Z]*s$"));
+    System.out.println(string2.matches("^[a-zA-Z]*s$"));
+        // Output: true
+        // Output: false
+
+     // $ Expression
+     // The $ boundary matcher an occurrence of a character/character class/group at the end of a line.
+    */
+    // Example 15
+    // Task: Your task is write a regex which will match word starting with vowel(a,e,i,o,A,E,I,O or U9
+    // The matched word can be of any length. The matcged word should consist of letters(lowercase and uppercase both)
+    // The matched word must start and end with a word boundary.
+
+    String string = "Found any match?";
+    String string2 = "Yes of course";
+
+     Answer == "\\b[aeiouAEIOU][a-zA-Z]*\\b"
+
+    // \b assert position at a word boundary.
+    // Three different positions qualify for word boundaries:
+    // * Before the first character in the string,if the first character is a word character.
+    // * Between two characters in the string,where one is a word character and the other is not a word character.
+    // * After the last character in the string,if the last character is a word character.   
+        
 
     }
 }
